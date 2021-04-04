c47da17f05f276f8850b3902e2363ac4b9f672b4
Lindsey Krause <79954805+LindseyKrause@users.noreply.github.com>
2021-04-03 18:24:50 -0700

    Folderchanges?

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,13 @@
 
-var promptUppercase;
-var promptNumeric;
-var promptPlength;
-var promptSpecial;
-var promptPlength;
+var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var special = ["!", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"]
+var characterSet = "";
+var randomPassword = "";
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-var generatePw = ({
+var generatePassword = ({
   //Choose Password Length between 8-128 characters
   promptPlength = function () {
     parseInt(prompt("Choose password length between 8-128 characters"));
@@ -19,39 +20,30 @@ var generatePw = ({
       enter = parseInt((prompt("You must choose between 8 and 128")));
     } else {
       // Continues once user input is validated
+      PromptLowercase = confirm("Will this contain Lowercase letters?");
+      promptUppercase = confirm("Will this contain Uppercase letters?");
       promptNumeric = confirm("Will this contain numbers?");
       promptSpecial = confirm("Will this contain special characters?");
-      promptUppercase = confirm("Will this contain Uppercase letters?");
-      PromptLowercase = confirm("Will this contain Lowercase letters?");
       //input validated to include at least one of the criteria
       if (promptLowercase && promptUppercase && promptNumeric && promptSpecial === false) {
         alert("You must choose at least one password criteria");
         promptCriteria();
       }
-      //only letters 
-      if (promptNumeric === true && (promptSpecial, promptUppercase, promptLowercase) === false) {
-        var numericPassword = document.getElementById("password").value; 
-        for (var i = 0; i < (numericPassword * 10); i++) {
-          // Generate a random number between 1 and 10
-          // Math.floor will round down, meaning we would get a number between 0 and 9, so we'll always add 1 to bump it up.
-          var num = Math.floor(Math.random() * 10) + 1;
+      //only Lowerletters 
+      if (promptLowercase === true) {
+        randomPassword = characterSet + lower;
+        console.log;
       }
     }
   }
 }
-    // Write password to the #password input
-    //function writePassword() {
-    //var password = generatePassword();
-    //var passwordText = document.querySelector("#password");
-    //Prompts for password criteria
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
 
-
-    //Password Generated to match Criteria
-    //Password displayed in alert or written to the page
-
-    //passwordText.value = password;
-
-
-    // Add event listener to generate button
-    generateBtn.addEventListener("click", generatePw),
-  //writePassword();
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+generatePassword();
